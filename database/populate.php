96df45b19e25df8febfd395d7aacb66ba0d75305
Adhi Setyawan <stw.adhi@gmail.com>
2021-09-28 08:20:21 +0700

    setup system, create fake data to POC.

--- database/populate.php
+++ database/populate.php
@@ -9,7 +9,7 @@
 (PHP_SAPI !== 'cli' || isset($_SERVER['HTTP_USER_AGENT'])) && die('cli only');
 use Faker\Factory;
 require_once 'bootsrap.php';
-use NilPortugues\Sql\QueryBuilder\Builder\GenericBuilder;
+use NilPortugues\Sql\QueryBuilder\Builder\MySqlBuilder;
 
 
 Class Populator 
@@ -20,8 +20,9 @@ Class Populator
     public function __construct($conn)
     {
         $this->faker = Factory::create('id_ID');
-        $this->genericBuilder = new GenericBuilder();
+        $this->genericBuilder = new MySqlBuilder();
         $this->conn = $conn;
+        $this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
     }
     /**
      * generate product 
@@ -54,19 +55,62 @@ Class Populator
      {
         for ($i=0;$i<10;$i++)
         {
+            // select random user
             $query = "select count(*) as 'count' from users";
             $stmt = $this->conn->query($query);
             $count = $stmt->fetch();
-            
-            
-            $user = $this->genericBuilder->select()
+            if ($count['count'] < 1) return false;
+            $rand = $this->faker->numberBetween(1,$count['count']);
+            $userQuery = $this->genericBuilder->select()
             ->setTable('users')
-            ->setColumn(['id'])
-            ->limit(1)
             ->where()
-            ->equals('id',$this->faker->numberBetween(0,$count['count']))
+            ->equals('id',$rand)
+            ->end();
+
+            $userQuery->limit(0,1);
+            $sql = $this->genericBuilder->write($userQuery);
+            $value = $this->genericBuilder->getValues();
+            $sth = $this->conn->prepare($sql);
+            $sth->execute($this->genericBuilder->getValues());
+            $user = $sth->fetch();
+            
+            // select random product
+            $query = "select count(*) as 'count' from products";
+            $stmt = $this->conn->query($query);
+            $count = $stmt->fetch();
+            $rand = $this->faker->numberBetween(1,$count['count']);
+            $productQuery = $this->genericBuilder->select()
+            ->setTable('products')
+            ->limit(0,1)
+            ->where()
+            ->equals('id',$rand)
             ->end();
             
+            $sql = $this->genericBuilder->write($productQuery);
+            $value = $this->genericBuilder->getValues();
+            $stat = $this->conn->prepare($sql);
+            $stat->execute($value);
+            $product = $stat->fetch();
+            
+            // emulate customer put thing into chart.
+            for ($j=0;$j<rand(3,9);$j++)
+            {
+                // avoid user put product in chart twice or more
+                $sql = "select * from carts where user_id = ? and product_id = ? limit 1";
+                $stm = $this->conn->prepare($sql);
+                $stm->execute([$user['id'],$product['id']]);
+                $cart = $stm->fetch();
+                if ($cart) continue;
+                $insert = $this->genericBuilder->insert()
+                ->setTable('carts')
+                ->setValues([
+                    'product_id' => $product['id'],
+                    'user_id' => $user['id'],
+                    'amount' => floor((rand(10,30) / 100) * $product['stock'])
+                ]);
+                $sql = $this->genericBuilder->writeFormatted($insert);
+                $this->conn->prepare($sql)->execute($this->genericBuilder->getValues());
+            }
         }
      }
 
@@ -96,10 +140,13 @@ Class Populator
 }
 
 $populator = new Populator($conn);
-$populator->cart();
+
 // populate user table
-//$populator->user();
+$populator->user();
 
 // populate product table
-//$populator->product();
+$populator->product();
+
+// populate cart 
+$populator->cart();
 
